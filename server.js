b9ff407048c90863ff17aad6a2f906397391ddfa
Huma88 <alejandro_el_Maki@hotmail.com>
2017-10-27 01:46:32 +0200

    fixed chat problems, and also added a box for the messages

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require("express");
 var app = express();
-var server = require("http").createServer(app);
-var io = require("socket.io").listen(server);
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
+app.use(express.static(__dirname+'/public'))
 
 users = [];
 connections = [];
